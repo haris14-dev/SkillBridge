474969e572d26a15af8728b6ad95ff3408a8c0a0
Haris Asif <harisasif761@gmail.com>
2026-06-04 01:05:11 +0500

    Update CORS middleware allowed origins
    Removed trailing slashes from allowed origins in CORS settings.

--- api.py
+++ api.py
@@ -7,9 +7,9 @@ app = FastAPI()
 
 app.add_middleware(    #for frontend and backend to communicate with each other without any CORS issues, we can add the CORSMiddleware to our FastAPI application. This middleware allows us to specify which origins are allowed to access our API, as well as which HTTP methods and headers are permitted. In this case, we are allowing all origins, credentials, methods, and headers for simplicity, but in a production environment, you may want to restrict these settings for better security.
     CORSMiddleware,
-    allow_origins=["https://skill-bridge-git-main-haris14-devs-projects.vercel.app/",
-                  "https://skill-bridge-two-gold.vercel.app/",
-    "https://skill-bridge-laf23kdzl-haris14-devs-projects.vercel.app/"],
+    allow_origins=["https://skill-bridge-git-main-haris14-devs-projects.vercel.app",
+                  "https://skill-bridge-two-gold.vercel.app",
+    "https://skill-bridge-laf23kdzl-haris14-devs-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
